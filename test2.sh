587a75da9f7c061c21f0ccdf1761100cf03490cb
NicolasVidal-Ch <69898768+NicolasVidal-Ch@users.noreply.github.com>
2020-09-03 10:18:19 +0200

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -15,7 +15,7 @@ echo	gateway 192.168.240.1 >> /etc/network/interfaces
 systemctl restart networking.service
 
 #Create a variable for change IP:
-COUNTER=1
+COUNTER=2
 NETWORK=192.168.240
 while [ $COUNTER -lt 254 ]
 do
@@ -48,7 +48,7 @@ ifconfig eth0 0.0.0.0
 systemctl restart networking.service
 
 #install sudo:
-apt install sudo mc
+apt install -y sudo mc
 
 #Prepare for install:
 apt install -y wget
@@ -57,6 +57,6 @@ wget --no-check-certificate https://github.com/NicolasVidal-Ch/Script-test/archi
 #Unzip the archive:
 tar xvf 004.tar.gz
 chmod +x Script-test-004/test.sh
-sh /Script-test-004/test.sh
+sh Script-test-004/test.sh
 
                 
